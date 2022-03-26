acb0e3a90025488c0ba57688e7dd2396953eef51
Sairkko <bastien.98@orange.fr>
2022-03-26 15:18:06 +0100

    Update Couleur, Footer couleur faite + j'ai changé les info Navbar relier avec ses sections + couleurs changé Animation AOS faite Il manque a faire la page contact (changé le html par un autre style) J'ai pas réussi à enlever la barre quand tu clique dans la partie activité

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-import AOS from 'aos';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.css';
@@ -7,9 +6,13 @@ import '../js/question_show';
 import '../js/scroll';
 import '../js/init';
 import '../js/activities';
+import AOS from 'aos';
+
+AOS.init();
 
 
 
 // start the Stimulus application
 import '../bootstrap';
+import 'jquery';
 
